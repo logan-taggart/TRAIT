f178893c6dd4ce154b643282501e80815e3a9daa
Logan Taggart <ltaggart1@ewu.edu>
2025-06-03 11:09:32 -0700

    Added Project Files

--- TRAIT-Front/main.js
+++ TRAIT-Front/main.js
@@ -1,4 +1,5 @@
 import { app, BrowserWindow } from 'electron';
+import { spawn } from 'child_process';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import url from 'url';
@@ -7,9 +8,31 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let mainWindow;
+let backendProcess;
+
 const isDev = !app.isPackaged;
+const platform = process.platform;
 
 app.on('ready', () => {
+    console.log('Starting backend...');
+
+    const exeName = platform === 'win32' ? 'backend.exe' : 'backend';
+    const exePath = isDev
+        ? path.join(__dirname, 'backend', exeName)
+        : path.join(process.resourcesPath, 'backend', exeName);
+
+    backendProcess = spawn(exePath, {
+        shell: true,
+        detached: false,
+        stdio: 'inherit',
+        windowsHide: true,
+        env: {
+            ...process.env,
+            ENV: isDev ? 'dev' : 'prod',
+        },
+    });
+
+
     console.log('Launching Electron window...');
 
     mainWindow = new BrowserWindow({
@@ -34,11 +57,22 @@ app.on('ready', () => {
         console.log('Electron window closed');
         mainWindow = null;
     });
+
+    mainWindow.on('close', () => {
+        console.log('Closing Electron...');
+        if (backendProcess) {
+            console.log('Sending SIGTERM to backend...');
+            backendProcess.kill('SIGTERM');
+        }
+    });
 });
 
 app.on('quit', () => {
     console.log('Electron is quitting...');
-    app.quit();
+    if (backendProcess) {
+        console.log('Sending SIGTERM to backend...');
+        backendProcess.kill('SIGTERM');
+    }
 });
 
 app.on('window-all-closed', () => {
